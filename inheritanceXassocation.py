a07390da4f007b809fe8f3fbb0578ebf16ba7421
AbdifatahJama <76138022+AbdifatahJama@users.noreply.github.com>
2021-06-14 14:40:30 +0100

    Add files via upload

---
+++ inheritanceXassocation.py
@@ -0,0 +1,11 @@
+'''When using object orientated programming within scripts there are to ideas that can be used
+
+
+Inheritance - when object follow an is a relationship
+Compisition - when object follow a has a relationship
+
+
+Association  - is a relationship between two classes, where one class is a component of another class. Such as a cube has a sqaure surface. Hence, we can use the sqaure class
+
+'''
+
